7a1f12e597fbb67b02ae7acad132ea2f5195d30c
Ayobami <ayobamidele006@gmail.com>
2022-10-08 09:38:15 +0100

    add week-3 file

--- week-2/carg_practice.c
+++ week-2/carg_practice.c
@@ -5,9 +5,9 @@ int main(int argc, string argv[])
 {
     // this is a condition that print hello and the second arg from user
     // i.e if arg count is equal to 2 print hello and the second arg provided by the user in the command line
-    if (argc == 2)
+    if (argc == 3)
     {
-        printf("hello, %s\n", argv[1]);
+        printf("hello, %s %s\n", argv[1], argv[2]);
     }
 
     // else just print hello, world
@@ -16,6 +16,3 @@ int main(int argc, string argv[])
         printf("hello, world\n");
     }
 }
-
-// Note the command line args are in string array called arg vector aka argv[]
-// array index starts at 0 which is why last element of argv is argc -1
