051269c1c15a00238a433d88b476d2fd606594e4
Ayobami <ayobamidele006@gmail.com>
2022-10-08 09:55:44 +0100

    add some comments to argv.c file

--- week-2/argv.c
+++ week-2/argv.c
@@ -3,13 +3,19 @@
 
 int main(int argc, string argv[])
 {
+    // this is a condition that print hello and the second arg from user
+    // i.e if arg count is equal to 2 print hello and the second arg provided by the user in the command line
     if (argc == 2)
     {
         printf("hello, %s\n", argv[1]);
     }
 
+    // else just print hello, world
     else
     {
         printf("hello, world\n");
     }
 }
+
+// Note the command line arg is an array of strings and the index of the first element is 0 which in this case is hello
+// Thus index 1 will be what the user typed as second arg in the command line.
