035601a99db459cb7565e45ee5bb8c4f56aea1f2
Ayobami <ayobamidele006@gmail.com>
2022-11-01 09:04:53 +0100

    my recent work

--- week-4/address_pa.c
+++ week-4/address_pa.c
@@ -6,6 +6,7 @@
 int main(void)
 {
     char *s = "HI!";
+
     printf("%c\n", *s);       /*Prints out the character in the address*/
     printf("%c\n", *(s + 1)); /*prints out the second character*/
     printf("%c\n", *(s + 2)); /*prints out the third character*/
